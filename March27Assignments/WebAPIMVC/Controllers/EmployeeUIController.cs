4f4bc46d8b628653baa4a3a19c8865272926782e
udayasri-28 <udayasri682@gmail.com>
2026-03-28 09:38:21 +0530

    Added Assignment

---
+++ March27Assignments/WebAPIMVC/Controllers/EmployeeUIController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIMVC.Controllers
+{
+    public class EmployeeUIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+        public IActionResult Details()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+        public IActionResult Export()
+        {
+            return View();
+        }
+    }
+}
---
+++ March27Assignments/WebAPIMVC/Migrations/20260325061703_imageadded.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPIMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class imageadded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "employees",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "employees");
+        }
+    }
+}
